1906d425701bae0de8d81cb5efef35c191c18536
Ismox1440 <ismael.saragusti24@gmail.com>
2023-03-11 00:26:54 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { PrismaClient } from '@prisma/client';
+import routes from './routes';
+import * as dotenv from 'dotenv';
+dotenv.config()
+
+const app = express();
+const prisma = new PrismaClient();
+const port = process.env.PORT || 5000;
+
+// Middlewares
+app.use(cors());    
+app.use(bodyParser.json());
+
+// Routes
+app.use('/', routes);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
+
+export default app
