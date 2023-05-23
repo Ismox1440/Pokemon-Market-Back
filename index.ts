ee8695e1510b9c5fe5119ba736a862ee6275d23f
Ismox1440 <ismael.saragusti24@gmail.com>
2023-05-23 12:47:37 -0300

    change index

--- index.ts
+++
@@ -1,9 +0,0 @@
-import app from "./src/app";
-import * as dotenv from "dotenv";
-dotenv.config();
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,25 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import routes from "./routes";
 import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 mongoose.connect(process.env.DATABASE_URL ?? "");
 
+const PORT = process.env.PORT || 5000;
+
+
+
 // Middlewares
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
 
 // Routes
 app.use("/", routes);
 
-export default app
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
